d79bffb96cffe46466cf04bef940de7a3b652fd2
05-1 <85896002+05-1@users.noreply.github.com>
2025-05-16 22:55:37 -0400

    Update therm.h

--- Engineer_Project_1.1/therm.h
+++ Engineer_Project_1.1/therm.h
@@ -4,68 +4,61 @@
 #include <SPI.h>
 #include <SD.h>
 
-// which analog pin to connect
-#define THERMISTORPIN A2         
-// resistance at 25 degrees C
-#define THERMISTORNOMINAL 10000      
-// temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25   
-// how many samples to take and average, more takes longer
-// but is more 'smooth'
-#define NUMSAMPLES 5
-// The beta coefficient of the thermistor (usually 3000-4000)
-#define BCOEFFICIENT 3950
-// the value of the 'other' resistor
-#define SERIESRESISTOR 10000    
+// Analog pin connected to the thermistor
+#define THERMISTORPIN A2
+#define THERMISTORNOMINAL 10000        // Nominal resistance at 25°C
+#define TEMPERATURENOMINAL 25          // Nominal temperature in Celsius
+#define NUMSAMPLES 5                   // Number of samples for averaging
+#define BCOEFFICIENT 3950              // Beta coefficient of thermistor
+#define SERIESRESISTOR 10000           // Series resistor value
 
-static float steinhart;
+static float steinhart;               // Global temperature result variable
 
+int samples[NUMSAMPLES];              // Buffer for thermistor readings
 
-int samples[NUMSAMPLES];
-class therm{
+// Class for reading and processing thermistor values
+class therm {
 public:
-therm(){
-  analogReference(AR_EXTERNAL);
-}
-
-void Therm_run() {
-  uint8_t i;
-  float average;
-
-  // take N samples in a row, with a slight delay
-  for (i=0; i< NUMSAMPLES; i++) {
-   samples[i] = analogRead(THERMISTORPIN);
-   delay(10);
+  therm() {
+    analogReference(AR_EXTERNAL);     // Use external analog reference voltage
   }
-  
-  // average all the samples out
-  average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
-     average += samples[i];
+
+  void Therm_run() {
+    uint8_t i;
+    float average;
+
+    // Take multiple samples from thermistor
+    for (i = 0; i < NUMSAMPLES; i++) {
+      samples[i] = analogRead(THERMISTORPIN);
+      delay(10);
+    }
+
+    // Compute average analog reading
+    average = 0;
+    for (i = 0; i < NUMSAMPLES; i++) {
+      average += samples[i];
+    }
+    average /= NUMSAMPLES;
+
+    Serial.print("Average analog reading ");
+    Serial.println(average);
+
+    // Convert ADC value to resistance
+    average = 1023 / average - 1;
+    average = SERIESRESISTOR / average;
+    Serial.print("Thermistor resistance ");
+    Serial.println(average);
+
+    // Apply Steinhart-Hart equation to convert resistance to temperature
+    steinhart = average / THERMISTORNOMINAL;
+    steinhart = log(steinhart);
+    steinhart /= BCOEFFICIENT;
+    steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15);
+    steinhart = 1.0 / steinhart;
+    steinhart -= 273.15;  // Convert from Kelvin to Celsius
+
+    Serial.print("Temperature ");
+    Serial.print(steinhart);
+    Serial.println(" *C");
   }
-  average /= NUMSAMPLES;
-
-  Serial.print("Average analog reading "); 
-  Serial.println(average);
-  
-  // convert the value to resistance
-  average = 1023 / average - 1;
-  average = SERIESRESISTOR / average;
-  Serial.print("Thermistor resistance "); 
-  Serial.println(average);
-  
-  
-  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart -= 273.15;                         // convert absolute temp to C
-  
-  Serial.print("Temperature "); 
-  Serial.print(steinhart);
-  Serial.println(" *C");
-  
-
-}
 };
